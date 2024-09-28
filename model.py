f67c631dfee3ced1b087b0133f98f9403b039795
Mohit_vaidya <mohiiitvaidya@gmail.com>
2024-09-28 20:08:33 +0530

    Story and image prompt both are generated by a fine tuned model now!!

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 import streamlit as st
-from prompts import few_shot_prompt_template_image, get_story_list
 from dotenv import load_dotenv
 import os
 
@@ -15,30 +14,34 @@ def initialize_llm():
     return ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
 
 
-# Generate full content: story, story list, and image prompt
 def generate_content(topic):
     story_model = genai.GenerativeModel(model_name="tunedModels/socialstories-my8gxcmyb2vx", )
-
-    llm = initialize_llm()
-
-    # Generate the story
-    # prompt_story = few_shot_prompt_template.format(topic=topic)
-    # story = llm.invoke(prompt_story).content
+    image_prompt_model = genai.GenerativeModel(model_name="tunedModels/imagepromptsgeneration-n56rok7p5r5v", )
 
     story_session = story_model.start_chat(
         history=[
         ]
     )
-    story = story_session.send_message(f"Generate a social story on topic {topic}").text
-    # Generate the image prompt
-    prompt_image = few_shot_prompt_template_image.format(story=story)
-    image_prompt = llm.invoke(prompt_image).content
 
-    # Split the story into two parts
-    story_list = get_story_list(story)
+    image_prompt_session = image_prompt_model.start_chat(
+        history=[
+        ]
+    )
+
+    story = story_session.send_message(f"Generate a social story on topic {topic}").text
+
+    image_prompt = image_prompt_session.send_message(story).text
 
     return {
         'story': story,
-        'story_list': story_list,
+        'story_list': get_story_list(story),
         'image_prompt': image_prompt
     }
+
+
+def get_story_list(story):
+    sentences = story.split('.')
+    sentences = [s.strip() for s in sentences if s.strip()]
+    sentences = [s + '.' for s in sentences]
+    n = len(sentences) // 2
+    return [" ".join(sentences[:n]), " ".join(sentences[n:])]
